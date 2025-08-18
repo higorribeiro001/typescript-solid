dcc740a70e1acebf70612695a4c111ccb8d42036
Higor Araujo <higorribeiroaraujo007@gmail.com>
2025-08-18 13:46:27 -0300

    feat: vantagens e desvantagens do solid

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+/*
+Vantagens e desvantagens do S.O.L.I.D.
+
+Vantagens:
+- Código modular
+- Código reutilizável (D.R.Y - Don't repeat youself)
+- Código testável, baixo acoplamento
+- Baixo acoplamento e alta coesão
+- Código expansível
+- Separation of concerns (Separação de conceitos)
+- Fácil manutenção
+
+Desvantagens:
+- Complexidade
+- Quantidade de código digitado aumenta
+- Tempo de desenvolvimento aumenta muito
+- Tenha cuidados com: YAGNI (You Aren't Gonna Need It - você não irá precisar disto), KISS (Keep It Simple, Stupid! - mantenha simples, estupido!)
+
+MVP - produto mínimo viável para jogar a aplicação no ar
+*/
