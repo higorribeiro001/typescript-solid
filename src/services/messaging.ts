afe83458d564a7d740c9652bbbe99522c64dc4c2
Higor Araujo <higorribeiroaraujo007@gmail.com>
2025-08-19 08:00:17 -0300

    feat: testando Messaging

--- src/services/messaging.ts
+++ src/services/messaging.ts
@@ -3,6 +3,6 @@ import { MessagingProtocol } from "../classes/interfaces/messaging-protocol";
 export class Messaging implements MessagingProtocol {
   // também não é responsabilidade do carrinho e nem do pedido (order)
   sendMessage(msg: string): void {
-    console.log('Mensagem enviada: ', msg);
+    console.log('Mensagem enviada:', msg);
   }
 }
